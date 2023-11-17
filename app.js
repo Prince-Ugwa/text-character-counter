60276233928f26969a1eec8239dd29d4bea91f6f
Prince-Ugwa <odiniyaprinceugwa@gmail.com>
2023-11-17 04:54:23 +0100

    finishin js functionality

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const remainingCounter = document.querySelector("#remaining-counter");
 textarea.addEventListener("keyup", () => {
   updateCounter();
 });
+updateCounter();
 
 function updateCounter() {
   totalCounter.innerText = textarea.value.length;
+  remainingCounter.innerText =
+    textarea.getAttribute("maxLength") - textarea.value.length;
 }
